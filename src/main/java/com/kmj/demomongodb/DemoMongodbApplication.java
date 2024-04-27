ea9dd20ed4462151904ab786a52498e6cd9f5e6b
mjkou <mjkou@thoughtworks.com>
2024-04-27 15:59:38 +0800

    [Feature] init

--- src/main/java/com/kmj/demomongodb/DemoMongodbApplication.java
+++ src/main/java/com/kmj/demomongodb/DemoMongodbApplication.java
@@ -1,8 +1,11 @@
 package com.kmj.demomongodb;
 
+import com.kmj.demomongodb.database.repository.SpringDataRiceProductionRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableMongoRepositories(basePackageClasses = SpringDataRiceProductionRepository.class)
 @SpringBootApplication
 public class DemoMongodbApplication {
 
---
+++ src/main/java/com/kmj/demomongodb/database/repository/SpringDataRiceProductionRepository.java
@@ -0,0 +1,24 @@
+package com.kmj.demomongodb.database.repository;
+
+import com.kmj.demomongodb.database.document.RiceProductionData;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SpringDataRiceProductionRepository extends MongoRepository<RiceProductionData, String> {
+    Page<RiceProductionData> findByArea(final String area, final Pageable pageable);
+    @Query("{ 'value' : { $gte: ?0 } }")
+    Page<RiceProductionData> listByValueEqualOrHigher(final String value, final Pageable pageable);
+    @Query("{ 'value' : {$lte: ?0 } }")
+    Page<RiceProductionData> listByValueEqualOrLess(final String value, final Pageable pageable);
+    Page<RiceProductionData> findByValue(final String value, final Pageable pageable);
+    @Query("{$or:[{'area': ?0}, {'area': ?1 }]}")
+    Page<RiceProductionData> findByAreas(final String areaOne, final String areaTwo, final Pageable pageable);
+    @Query("{'value': {$in: [ ?0,  ?1 ]}}")
+    Page<RiceProductionData> findByValues(final String valueBegin, final String valueEnd, final Pageable pageable);
+    @Query("{'year': {$in: [ ?0,  ?1 ]}}")
+    Page<RiceProductionData> findByPeriod(final String yearBegin, final String yearEnd, final Pageable pageable);
+}
